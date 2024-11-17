565d6bda2604e77ea5f4816792cd079e4ceaa7f9
M Tarek <41743928+mutarek@users.noreply.github.com>
2024-11-17 09:41:04 +0600

    have to kotlin

--- app/src/main/java/com/dhakadigital/enterprisepaymentapicall_kotlin/network/ApiConfig.kt
+++ app/src/main/java/com/dhakadigital/enterprisepaymentapicall_kotlin/network/ApiConfig.kt
@@ -21,6 +21,7 @@ class ApiConfig {
             // Retrofit
             val retrofit = Retrofit.Builder()
                 // in this section we setup the Retrofit
+                //have to set
                 .baseUrl("http://finifyapi.redltd.tech:8000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
